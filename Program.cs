d5b0f19221cddb748a2d1883003f2d53066f6b25
Elazar <e0548448478@gmail.com>
2025-06-15 10:36:24 +0300

    srart project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InvestigationGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
